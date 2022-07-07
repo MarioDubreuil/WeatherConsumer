10c6401d622b3e770be8e4fe85e2e4abad998866
MarioDubreuil <mario_dubreuil@hotmail.com>
2022-07-06 20:42:52 -0400

    consume messages

--- WeatherConsumer/Program.cs
+++ WeatherConsumer/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ConsumerConfig
+{
+    GroupId = "weather-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("weather-topic");
+
+CancellationTokenSource token = new();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+        if (response.Message != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+            Console.WriteLine($"State: {weather.State}, Temperature: {weather.Temperature}F");
+        }
+    }
+}
+catch (Exception)
+{
+
+    throw;
+}
+
+public record Weather(string State, int Temperature);
